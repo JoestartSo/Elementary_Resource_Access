029b3bfa636511296a27e117ea45509bc29aab5d
JoestartSo <sujuechenhao@yahoo.com>
2020-07-15 15:26:13 +0930

    version 1

--- app/src/main/java/com/example/gainresouces/MainActivity.kt
+++ app/src/main/java/com/example/gainresouces/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.gainresouces
 
+import android.graphics.drawable.Drawable
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,12 +18,20 @@ class MainActivity : AppCompatActivity() {
         val imageViewAvatar=findViewById<ImageView>(R.id.image_avatar)
         val btnMale=findViewById<Button>(R.id.btn_male)
         val btnFemale=findViewById<Button>(R.id.btn_female)
+        val btnHello=findViewById<Button>(R.id.btn_hello)
 
         //get resouce id and use it directly
         btnMale.setOnClickListener({v->imageViewAvatar.setImageResource(R.drawable.male)})
 
-        //get resouce value by resouce id
-        var imageFemale=applicationContext.getDrawable(R.drawable.female)
+        //get resouce value by applicationContext property and resouce id
+        var imageFemale:Drawable?=applicationContext.getDrawable(R.drawable.female)
         btnFemale.setOnClickListener({v-> imageViewAvatar.setImageDrawable(imageFemale)})
+
+        //get resouce value by resouces property property and resouce id
+        val hello:String=resources.getString(R.string.hello)
+        var mToast=Toast.makeText(this,resources.getString(R.string.hello),Toast.LENGTH_SHORT)
+        mToast.setGravity(Gravity.CENTER,0,0);
+        btnHello.setOnClickListener({v->mToast.show() })
+
     }
 }
